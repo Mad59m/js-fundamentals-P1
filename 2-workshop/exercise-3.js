4fd83c2fccf1655e2f2d4fd220e242b8b1e5cab5
Madjid Mehrdoost <madjid59m@gmail.com>
2021-09-26 21:47:43 -0400

    jsP1 - Ex. 3

--- 2-workshop/exercise-3.js
+++ 2-workshop/exercise-3.js
@@ -6,11 +6,14 @@ function exercise3() {
   // It is should print to the console the numbers 1, 2, 3, 4, 5.
   //
   // Edit only the code between the lines (below)
-  // ----------------------------------------------------------------
-    for (let number = 2; number < 7; number++) 
-    {
-      console.log(number);
-      }
+  // -----------------------------------------------------------------
+  for (let number = 2; number < 7; number++) {
+    console.log(number);
+  }
+
+  for (let number = 1; number < 6; number++) {
+    console.log(number);
+  }
   // -----------------------------------------------------------------
   // Edit only the code between the lines (above)
   //
