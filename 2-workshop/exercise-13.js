b6f82f6416ba9fa17cb469b60d5c2935bd4a23a5
Madjid Mehrdoost <madjid59m@email.com>
2021-09-27 20:30:00 -0400

    jsP1 - Ex. 13

--- 2-workshop/exercise-13.js
+++ 2-workshop/exercise-13.js
@@ -30,7 +30,19 @@ for(let i=3; i<=50; i++)
   second=number;
 }
 console.log(number);
-  // -----------------------------------------------------------------
+  //
+  //another way
+  var i;
+var fib = []; // Initialize array!
+
+fib[0] = 0;
+fib[1] = 1;
+for (i = 2; i <= 50; i++) {
+  // Next fibonacci number = previous + one before previous
+  // Translated to JavaScript:
+  fib[i] = fib[i - 2] + fib[i - 1];
+  console.log(fib[i]);
+}
   // Write code between the lines (above)
   //
   //
