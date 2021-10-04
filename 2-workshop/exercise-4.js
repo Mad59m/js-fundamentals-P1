7f94501921b43af9718f9e816f30864e104a0c94
Madjid Mehrdoost <madjid59m@email.com>
2021-10-04 19:57:09 -0400

    JS-P1-Ex.4 minor change applied

--- 2-workshop/exercise-4.js
+++ 2-workshop/exercise-4.js
@@ -9,12 +9,9 @@ function exercise4() {
   //
   // Edit only the code between the lines (below)
   // -----------------------------------------------------------------
-  for (let number = 0; number < 12; number++) {
+  for (let number = 0; number <= 12; number++) {
+    let square = number*number
     console.log(square);
-  }
-
-  for (let number = 1; number < 6; number++) {
-    console.log(number);
   }
   // -----------------------------------------------------------------
   // Edit only the code between the lines (above)
